598208a352a55f9394acf570af34a577638b7d18
Angelia-cj <cjia0620@gmail.com>
2021-07-11 17:50:20 +0800

    打包文件分析与优化

--- workspace/cjshop-client/src/fiters/index.js
+++ workspace/cjshop-client/src/fiters/index.js
@@ -1,8 +1,16 @@
 // 自定义过滤器模块
 
-import Vue from 'vue'
+// moment
+/* import Vue from 'vue'
 import moment from 'moment'
 
 Vue.filter('dateString', function (value, format) {
   return moment(value).format(format || 'YYYY-MM-DD HH:mm:ss')
+}) */
+
+import Vue from 'vue'
+import format from 'date-fns/format'
+
+Vue.filter('dateString', function (value, formatStr = 'YYYY-MM-DD HH:mm:ss') {
+  return format(value, formatStr)
 })
