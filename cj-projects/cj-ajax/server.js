f8de0bade068a8923a61d689a5480e7969227cec
Angelia-cj <cjia0620@gmail.com>
2021-09-27 20:11:00 +0800

    测试

--- cj-projects/cj-ajax/server.js
+++ cj-projects/cj-ajax/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+// 暴露静态资源
+app.use(express.static(__dirname + 'src'))
+
 // 路由
 app.get('/test_get', (request, response) => {
     response.send('hello_test_get')
@@ -8,6 +11,7 @@ app.get('/test_get', (request, response) => {
 
 app.listen(8080, (err) => {
     if (!err) {
-        console.log('测试ajax请求的服务器开启成功了');
+        console.log('测试ajax请求的服务器开启成功了，测试地址如下：');
+        console.log('http://127.0.0.1:8081/1_ajax小试牛刀.html');
     }
 })
