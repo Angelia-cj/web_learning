fbb271760a73c1ecb6a98ebed3fe46898cdeef44
Angelia-cj <cjia0620@gmail.com>
2021-10-25 22:43:13 +0800

    15_cors解决跨域

--- cj-projects/cj-ajax/server.js
+++ cj-projects/cj-ajax/server.js
@@ -1,11 +1,15 @@
 // 引入express
 const express = require('express')
+const cors = require('cors')
 
 // 创建app实例对象
 const app = express()
 
 // post请求时，使用中间件解析urlencoded形式的body请求体参数
 app.use(express.urlencoded({ extended: true }))
+
+// 使用cors中间件解决跨域问题
+app.use(cors())
 
 // post请求时，使用中间件解析json形式的body请求体参数
 app.use(express.json())
@@ -16,14 +20,31 @@ app.use(express.static(__dirname + '/src'))
 // 响应GET请求 ----- 可以接收query参数
 app.get('/test_get', (request, response) => {
     console.log('有人请求test_get了--携带的query参数是：', request.query);
-    request.setHeader('Access-Control-Allow-Origin', '*')
+    // response.setHeader('Access-Control-Allow-Origin', '*');
+    // response.setHeader('Access-Control-Expose-Headers', '*');
+    // request.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8080')
     response.send('hello_test_get')
+})
+
+// 欲请求的方式(检查服务器性能)
+/* app.options('/test_put', (request, response) => {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Expose-Headers', '*');
+    response.setHeader('Access-Control-Allow-methods', '*');
+    response.send()
+}) */
+
+// 响应put请求
+app.put('/test_put', (request, response) => {
+    // response.setHeader('Access-Control-Allow-Origin', '*');
+    // response.setHeader('Access-Control-Expose-Headers', '*');
+    response.send("hello_test_put")
 })
 
 // 响应Get请求 -----可以接收params参数
 app.get('/test_get2/:name/:age', (request, response) => {
     console.log('有人请求test_get2了', request.params);
-    request.setHeader("Access-Control-Allow-Origin", '*')
+    response.setHeader("Access-Control-Allow-Origin", '*')
     response.send('hello_test_get2')
 })
 
